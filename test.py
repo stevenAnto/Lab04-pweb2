a61d28384378011127f9c8c2eb0e5f7613be1c89
HenryGalvezQ <hgalvezq@unsa.edu.pe>
2022-06-01 22:34:09 -0500

    test de pruebas

---
+++ test.py
@@ -0,0 +1,7 @@
+from chessPictures import *
+from interpreter import draw
+from picture import *
+#draw(rock.verticalMirror())
+#draw(rock.up(bishop))
+#draw(rock.join(bishop.join(knight)))
+draw(rock.horizontalRepeat(4))
