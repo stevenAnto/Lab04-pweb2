cba027e12fdffce1fed6c87fa06fe23245e9efe3
Henry Galvez <90292000+HenryGalvezQ@users.noreply.github.com>
2022-06-05 23:12:28 -0500

    Update Ejercicio2f.py

--- Ejercicio2f.py
+++ Ejercicio2f.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+from picture import *
+
+fila=square.join(square.negative()).horizontalRepeat(4)
+draw(fila.under(fila.negative()).verticalRepeat(2))
